9a9a4af44a5cdb98c91244868be0cc11baeee573
lowfc <lowfc@mail.ru>
2021-08-03 12:03:12 +0500

    03 08 update here

--- ApiHandler.py
+++ ApiHandler.py
@@ -2,51 +2,143 @@ from aiohttp import web
 import json
 from CourseHolder import CourseHolder
 import argparse
-
-
-async def handle(request):
-    response = {'status': 'ok'}
-    return web.Response(text=json.dumps(response), status=200)
-
-
-'''
-app = web.Application()
-app.router.add_get('/', handle)
-web.run_app(app)
-'''
+import aiohttp
+import asyncio
+import logging
 
 
 class Main(CourseHolder):  # main class
     rubles = 0
     euros = 0
     dollars = 0
-    debug = False
+    aliases = {}
 
-    def __init__(self, course_update_cool_down, rubles, dollars, euros, debug):
-        '''if debug.lower() in ('1', 'true', 'y'):
-            self.debug = True
+    def __init__(self, course_update_cool_down, rubles, dollars, euros, debug, aliases=None):
+        if aliases is None:
+            logging.warning('aliases not provide')
+            aliases = {'eur': 'eur', 'rub': 'rub', 'usd': 'usd'}
         else:
-            self.debug = False'''
+            if 'eur' not in aliases:
+                aliases['eur'] = 'eur'
+            if 'rub' not in aliases:
+                aliases['rub'] = 'rub'
+            if 'usd' not in aliases:
+                aliases['usd'] = 'usd'
+
+        self.aliases = aliases
         self.dollars = dollars
         self.euros = euros
         self.rubles = rubles
-        super().__init__(course_update_cool_down)  # init master-constructor
+        logging.info('app started')
+
+        # start server
+        self.app = web.Application()
+        self.set_routes()
+        runner = aiohttp.web.AppRunner(self.app)
+        logging.info('server changed to up state')
+        asyncio.get_event_loop().run_until_complete(runner.setup())
+        site = aiohttp.web.TCPSite(runner)
+        asyncio.get_event_loop().run_until_complete(site.start())
+
+        try:
+            super().__init__(course_update_cool_down, debug)  # init master-constructor
+        except KeyboardInterrupt:
+            logging.warning('App closed by user request (Ctrl + C)')
+            print('Script killed by user request')
+            input()
+
+    def output_info_composer(self):
+        return '\n{rub_a}: {rub}\n{usd_a}: {usd}\n{eur_a}: {eur}\n\n'.format(
+            rub_a=self.aliases['rub'],
+            eur_a=self.aliases['eur'],
+            usd_a=self.aliases['usd'],
+            rub=self.rubles,
+            eur=self.euros,
+            usd=self.dollars
+        ) + '{rub_usd_a}:  {rub_usd}\n{rub_eur_a}: {rub_eur}\n{usd_eur_a}: {usd_eur}\n\n'.format(
+            rub_usd_a=self.aliases['rub'] + '-' + self.aliases['usd'],
+            rub_eur_a=self.aliases['rub'] + '-' + self.aliases['eur'],
+            usd_eur_a=self.aliases['usd'] + '-' + self.aliases['eur'],
+            rub_usd=self.dollars,
+            rub_eur=self.eur,
+            usd_eur=self.currency_ratio
+        ) + 'sum: {rub} {rub_a} / {usd} {usd_a} / {eur} {eur_a}\n'.format(
+            rub=self.rubles + self.dollars * self.dollar + self.euros * self.eur,
+            usd=self.dollars + self.rubles / self.dollar + self.euros / self.currency_ratio,
+            eur=self.euros + self.rubles / self.eur + self.dollars * self.currency_ratio,
+            rub_a=self.aliases['rub'],
+            eur_a=self.aliases['eur'],
+            usd_a=self.aliases['usd']
+        )
 
     def print_course(self):  # overrides course print
-        print('rub: {}\nusd: {}\neur: {}\n'.format(self.rubles, self.dollars, self.euros))
-        print('rub-usd: {}\nrub-eur: {}\nusd-eur: {}\n'.format(
-            self.dollar,
-            self.eur,
-            self.currency_ratio
-        ))
-        print('sum: {} rub / {} usd / {} eur'.format(
-            self.rubles + self.dollars * self.dollar + self.euros * self.eur,
-            self.dollars + self.rubles / self.dollar + self.euros / self.currency_ratio,
-            self.euros + self.rubles / self.eur + self.dollars * self.currency_ratio,
-        ))
+        if not self.debug and self.course_changed:
+            print(self.output_info_composer())
+            self.course_changed = False
+
+    def set_routes(self):
+        self.app.router.add_get('/{valuate}/{method}', self.handler)
+        self.app.router.add_post('/{valuate}/{method}', self.handler)
+        self.app.router.add_post('/{valuate}', self.handler)
+        logging.info('routes declared on server')
+
+    async def handler(self, request):
+        response = {}
+        query = str(request.rel_url)
+        if self.debug:
+            print('An API request was noticed\nrequest:')
+        if request.method == 'GET':
+            if self.debug:
+                print(request.url)
+            if query == '/usd/get':
+                response['usd'] = self.dollars
+            elif query == '/rub/get':
+                response['rub'] = self.rubles
+            elif query == '/eur/get':
+                response['eur'] = self.euros
+            elif query == '/amount/get':
+                response = self.output_info_composer()
+                return web.Response(text=response, status=200)
+        else:
+            data = await request.json()
+            self.course_changed = True
+            if self.debug:
+                print(request.url, json.dumps(data))
+            response['new_values'] = {}
+            if query == '/amount/set':
+                if len(data.keys())>0:
+                    if 'rub' in data.keys():
+                        self.rubles = data['rub']
+                        response['new_values']['rub'] = self.rubles
+                    if 'usd' in data.keys():
+                        self.dollars = data['usd']
+                        response['new_values']['usd'] = self.dollars
+                    if 'eur' in data.keys():
+                        self.euros = data['eur']
+                        response['new_values']['eur'] = self.euros
+                else:
+                    logging.warning('API requested at POST without parameters')
+            elif query == '/modify':
+                if 'rub' in data.keys():
+                    self.rubles += data['rub']
+                    response['new_values']['rub'] = self.rubles
+                if 'usd' in data.keys():
+                    self.dollars += data['usd']
+                    response['new_values']['usd'] = self.dollars
+                if 'eur' in data.keys():
+                    self.euros += data['eur']
+                    response['new_values']['eur'] = self.euros
+        logging.debug('API requested as '+str(request.method)+' with param '+str(request.rel_url)+' and responded like '
+                      + str(response))
+        if self.rubles < 0 or self.dollars < 0 or self.euros < 0:
+            logging.warning('One or more of the monetary units has taken a negative value')
+        if self.debug:
+            print('response:\n', json.dumps(response))
+        return web.Response(text=json.dumps(response), status=200, headers={'content-type': 'text/plain'})
 
 
 if __name__ == '__main__':
+    logging.basicConfig(filename="C:/Users/gagat/Desktop/ujin_microservice/debug.log", level=logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '--debug',
--- CourseHolder.py
+++ CourseHolder.py
@@ -1,6 +1,8 @@
 from abc import ABCMeta, abstractmethod
 import requests
 import asyncio
+import datetime
+import logging
 
 
 class CourseHolder:
@@ -9,29 +11,47 @@ class CourseHolder:
     dollar = 0
     course_update_cool_down = 0  # in minutes
     currency_ratio = 0
+    debug = False
+    course_changed = False
 
-    def __init__(self, course_update_cool_down):
+    def __init__(self, course_update_cool_down, debug):
+        if debug.lower() in ('1', 'true', 'y'):
+            self.debug = True
+        else:
+            self.debug = False
+        if not self.debug:
+            print('Application started')
         self.course_update_cool_down = course_update_cool_down
         self.check_course_stream = asyncio.get_event_loop()
         tasks = asyncio.wait([self.check_course_stream.create_task(self.check_course()),
                               self.check_course_stream.create_task(self.show_course())])
+        logging.debug('check course stream started')
         self.check_course_stream.run_until_complete(tasks)
+        self.check_course_stream.close()
 
     async def check_course(self):
-        print('requested cc')
-        response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')  # check course
-        self.eur = response.json()['Valute']['EUR']['Value']
-        self.dollar = response.json()['Valute']['USD']['Value']
-        self.currency_ratio = self.dollar / self.eur
-        await asyncio.sleep(self.course_update_cool_down )
+        logging.debug('valuate course requested from API')
+        last_course = (self.eur, self.dollar)
+        try:
+            response = requests.get('https://www.cbr-xml-daily.ru/daily_json.js')  # check course
+            self.eur = response.json()['Valute']['EUR']['Value']
+            self.dollar = response.json()['Valute']['USD']['Value']
+            self.currency_ratio = self.dollar / self.eur
+        except Exception as e:
+            logging.warning('The request on course API was receive with errors, the exchange rate was not replaced '
+                            '. Error: ' + str(e))
+        if (self.eur, self.dollar) != last_course:
+            self.course_changed = True
+        if not self.debug:
+            print('Valuate course updated at', datetime.datetime.now())
+        await asyncio.sleep(self.course_update_cool_down * 10)
         await self.check_course_stream.create_task(self.check_course())
 
     @abstractmethod
     def print_course(self):
         pass
-    #todo no clodes while stream awaiting
+
     async def show_course(self):
-        print('fc')
         self.print_course()
-        await asyncio.sleep(2)
+        await asyncio.sleep(10)
         await self.check_course_stream.create_task(self.show_course())
